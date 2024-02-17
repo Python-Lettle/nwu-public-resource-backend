3123183dd470101b7c177406ee9afc0702455f65
Lettle <1071445082@qq.com>
2024-02-17 16:25:00 +0800

    Add Library

---
+++ src/main/java/cn/lettle/pubresource/api/LibrarySeatApi.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Lettle
+ * Date: 2024/2/17
+ * Time: 15:57
+ * Description:
+ */
+
+package cn.lettle.pubresource.api;
+
+public class LibrarySeatApi {
+}
---
+++ src/main/java/cn/lettle/pubresource/entity/Library.java
@@ -0,0 +1,48 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Lettle
+ * Date: 2024/2/17
+ * Time: 16:02
+ * Description:
+ */
+
+package cn.lettle.pubresource.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class Library {
+
+    /** 三维数组: 楼层 行x 列y
+     *  一层楼:
+     *   0 0 0 0 0
+     *   1 1 1 0 0
+     *   0 0 0 0 0
+     */
+    private int [][][] seats;
+    /** 楼层数 **/
+    private int floor_num;
+
+    /**
+     * Occupy 占座函数
+     * @input floor: 楼层数, x:行, y:列
+     * @return boolean 占座是否成功
+     */
+    public boolean occupy (int floor, int x, int y)
+    {
+        if (this.seats[floor][x][y] == 0) {
+            this.seats[floor][x][y] = 1;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkSeat (int floor, int x, int y)
+    {
+        return seats[floor][x][y] == 1;
+    }
+}
